26098d6d5e5b4be5f2d6b4fa8f26c7e8a0f0609c
putuyoga <putuyoga@gmail.com>
2016-01-04 21:25:23 +0700

    Refactoring

--- WooCommerceAPIConsumer/Services/CustomerService.cs
+++ WooCommerceAPIConsumer/Services/CustomerService.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SharpCommerce.Services
@@ -16,65 +14,117 @@ namespace SharpCommerce.Services
         public CustomerService(WoocommerceApiDriver apiDriver)
             : base(apiDriver) { }
 
-        // Create A Customer
-        public Customer Create(Customer orderData)
+        /// <summary>
+        /// Create a Customer
+        /// </summary>
+        /// <param name="orderData"></param>
+        /// <returns></returns>
+        public async Task<Customer> Create(Customer orderData)
         {
-            return Post(apiEndpoint: "customers", toSerialize: new CustomerBundle { Content = orderData }).Content;
+            var bundle = new CustomerBundle { Content = orderData };
+            return (await Post(apiEndpoint: "customers", toSerialize: bundle)).Content;
         }
 
-        // View A Customer by id
-        public Customer Get(int customerId)
+        /// <summary>
+        /// View a Customer by ID
+        /// </summary>
+        /// <param name="customerId">The identifier of customer</param>
+        /// <returns></returns>
+        public async Task<Customer> Get(int customerId)
         {
-            return this.Get<CustomerBundle>(apiEndpoint: String.Format("customers/{0}", customerId)).Content;
+            var endPoint = string.Format("customers/{0}", customerId);
+            return (await Get<CustomerBundle>(endPoint)).Content;
         }
 
-        // View A Customer by Email
-        public Customer Get(string email)
+        /// <summary>
+        /// View a Customer by Email
+        /// </summary>
+        /// <param name="email">Email of customer</param>
+        /// <returns></returns>
+        public async Task<Customer> Get(string email)
         {
-            return this.Get<CustomerBundle>(apiEndpoint: String.Format("customers/email/{0}", email)).Content;
+            var endPoint = string.Format("customers/email/{0}", email);
+            return (await Get<CustomerBundle>(endPoint)).Content;
         }
 
-        // View List Of Customers
-        public IEnumerable<Customer> Get(Dictionary<string, string> parameters = null)
+        /// <summary>
+        /// View List of Customers
+        /// </summary>
+        /// <param name="parameters">Parameters to filter list of customers</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Customer>> Get(Dictionary<string, string> parameters = null)
         {
-            return Get<CustomersBundle>(apiEndpoint: "customers", parameters: parameters).Content;
+            return (await Get<CustomersBundle>(apiEndpoint: "customers", parameters: parameters)).Content;
         }
 
-        // Update A Customer
-        public Customer Update(int customerId, Customer newData)
+        /// <summary>
+        /// Update a Customer
+        /// </summary>
+        /// <param name="customerId">The identifier of customer</param>
+        /// <param name="newData">Customer object to be updated</param>
+        /// <returns></returns>
+        public async Task<Customer> Update(int customerId, Customer newData)
         {
-            return Put(apiEndpoint: String.Format("customers/{0}", customerId), toSerialize: new CustomerBundle { Content = newData }).Content;
+            var endPoint = String.Format("customers/{0}", customerId);
+            var bundle = new CustomerBundle { Content = newData };
+            return (await Put(endPoint, toSerialize: bundle)).Content;
         }
 
-        // Create/Update Multiple Customers
-        public IEnumerable<Customer> CreateUpdateMany(IEnumerable<Customer> ordersData)
+        /// <summary>
+        /// Create or Update Multiple Customers
+        /// </summary>
+        /// <param name="ordersData">List of customer object to be created or updated</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Customer>> CreateUpdateMany(IEnumerable<Customer> ordersData)
         {
-            return Put(apiEndpoint: "customers/bulk", toSerialize: new CustomersBundle { Content = ordersData }).Content;
+            var bundle = new CustomersBundle { Content = ordersData };
+            return (await Put(apiEndpoint: "customers/bulk", toSerialize: bundle)).Content;
         }
 
-        // Delete a Customer Permanently
-        public string DeletePermanently(int id)
+        /// <summary>
+        /// Delete a Customer Permanently
+        /// </summary>
+        /// <param name="id">The identifier of Customer</param>
+        /// <returns></returns>
+        public async Task<string> DeletePermanently(int id)
         {
-            return Delete<dynamic>(apiEndpoint: String.Format("customers/{0}", id)).message;
+            var endPoint = string.Format("customers/{0}", id);
+            return (await Delete<dynamic>(endPoint)).message;
         }
 
-        // View Customers Count
-        // Can have the 'role' filter for customers vs subscribers
-        public int Count(Dictionary<string, string> parameters = null)
+        /// <summary>
+        /// View Customers Count. 
+        /// Can have the 'role' filter for customers vs subscribers
+        /// </summary>
+        /// <param name="parameters">Parameter to filter list of customer </param>
+        /// <returns></returns>
+        public async Task<int> Count(Dictionary<string, string> parameters = null)
         {
-            return Get<dynamic>(apiEndpoint: "customers/count", parameters: parameters).count;
+            return (await Get<dynamic>(apiEndpoint: "customers/count", parameters: parameters)).count;
         }
 
-        // View Customer Orders
-        public IEnumerable<Order> GetOrders(int customerId, Dictionary<string, string> parameters = null)
+        /// <summary>
+        /// View Customer's Order
+        /// </summary>
+        /// <param name="customerId">The identifier of customer</param>
+        /// <param name="parameters">Parameter to filter list of orders</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Order>> GetOrders(int customerId, Dictionary<string, string> parameters = null)
         {
-            return Get<OrdersBundle>(apiEndpoint: String.Format("customers/{0}/orders", customerId), parameters: parameters).Content;
+            var endPoint = String.Format("customers/{0}/orders", customerId);
+            return (await Get<OrdersBundle>(endPoint, parameters: parameters)).Content;
         }
 
-        // View Customer Downloads
-        public IEnumerable<Download> GetDownloads(int customerId, Dictionary<string, string> parameters = null)
+        /// <summary>
+        /// View Customer's Download
+        /// </summary>
+        /// <param name="customerId">The identifier of customer</param>
+        /// <param name="parameters">Parameter to filter list of downloads</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Download>> GetDownloads(int customerId, Dictionary<string, string> parameters = null)
         {
-            return Get<DownloadsBundle>(apiEndpoint: String.Format("customers/{0}/downloads", customerId), parameters: parameters).Content;
+            var endPoint = String.Format("customers/{0}/downloads", customerId);
+            return (await Get<DownloadsBundle>(endPoint, parameters: parameters)).Content;
         }
     }
 }
