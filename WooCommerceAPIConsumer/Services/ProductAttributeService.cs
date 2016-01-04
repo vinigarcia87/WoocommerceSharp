daae5b022b4d297f913803b43f0f79efca3f4c35
putuyoga <putuyoga@gmail.com>
2016-01-04 16:45:43 +0700

    Refactoring

--- WooCommerceAPIConsumer/Services/ProductAttributeService.cs
+++ WooCommerceAPIConsumer/Services/ProductAttributeService.cs
@@ -5,6 +5,7 @@ namespace SharpCommerce.Services
 {
     using SharpCommerce.Data.Products;
     using SharpCommerce.Web;
+    using System.Threading.Tasks;
 
     public class ProductAttributeService : Service
     {
@@ -13,47 +14,76 @@ namespace SharpCommerce.Services
         public ProductAttributeService(WoocommerceApiDriver apiDriver)
             : base(apiDriver) { }
 
-        // Create a Product Attribute
-        public ProductAttribute Create(ProductAttribute productAttributeData)
+        /// <summary>
+        /// Create a Product Attribute 
+        /// </summary>
+        /// <param name="productAttributeData">Product attribute to be created</param>
+        /// <returns>Newly created product attribute object</returns>
+        public async Task<ProductAttribute> Create(ProductAttribute productAttributeData)
         {
-            return Post(apiEndpoint: BaseApiEndpoint, toSerialize: new ProductAttributeBundle { Content = productAttributeData }).Content;
+            var bundle = new ProductAttributeBundle { Content = productAttributeData };
+            return (await Post(apiEndpoint: BaseApiEndpoint, toSerialize: bundle)).Content;
         }
 
-        // View a Product Attribute
-        public ProductAttribute Get(int productAttributeId)
+        /// <summary>
+        /// View a Product Attribute
+        /// </summary>
+        /// <param name="productAttributeId">The identifiter of product attribute</param>
+        /// <returns>A product attribute object</returns>
+        public async Task<ProductAttribute> Get(int productAttributeId)
         {
-            return this.Get<ProductAttributeBundle>(apiEndpoint: String.Format("{0}/{1}", BaseApiEndpoint, productAttributeId)).Content;
+            var endPoint = String.Format("{0}/{1}", BaseApiEndpoint, productAttributeId);
+            return (await Get<ProductAttributeBundle>(endPoint)).Content;
         }
 
-        // View a List of Product Attribute
-        public IEnumerable<ProductAttribute> Get(Dictionary<string, string> parameters = null)
+        /// <summary>
+        /// View List of Product Attributes
+        /// </summary>
+        /// <param name="parameters">Parameter to filter list of product attributes</param>
+        /// <returns>List of product attributes object</returns>
+        public async Task<IEnumerable<ProductAttribute>> Get(Dictionary<string, string> parameters = null)
         {
-            return Get<ProductAttributesBundle>(apiEndpoint: BaseApiEndpoint, parameters: parameters).Content;
+            return (await Get<ProductAttributesBundle>(apiEndpoint: BaseApiEndpoint, parameters: parameters)).Content;
         }
 
-        // Update a Product Attribute
-        public ProductAttribute Update(int productAttributeId, ProductAttribute newData)
+        /// <summary>
+        /// Update a product attribute
+        /// </summary>
+        /// <param name="productAttributeId">The identifier of product attribute</param>
+        /// <param name="newData">The new data to be updated</param>
+        /// <returns>New product attribute object</returns>
+        public async Task<ProductAttribute> Update(int productAttributeId, ProductAttribute newData)
         {
-            return Put(apiEndpoint: String.Format("{0}/{1}", BaseApiEndpoint, productAttributeId), toSerialize: new ProductAttributeBundle { Content = newData }).Content;
+            var endPoint = String.Format("{0}/{1}", BaseApiEndpoint, productAttributeId);
+            var bundle = new ProductAttributeBundle { Content = newData };
+            return (await Put(endPoint, toSerialize: bundle)).Content;
         }
 
-        // Delete a Product Attribute
-        public string MoveToTrash(int id)
+        /// <summary>
+        /// Delete a product attribute
+        /// </summary>
+        /// <param name="id">The identifier of product attribute</param>
+        /// <returns></returns>
+        public async Task<string> MoveToTrash(int id)
         {
-            return Delete(id);
+            return await Delete(id);
         }
 
-        // Delete a Product Attribute Permanently
-        public string DeletePermanently(int id)
+        /// <summary>
+        /// Delete a Product Attribute Permanently
+        /// </summary>
+        /// <param name="id">The identifier of product attribute</param>
+        /// <returns></returns>
+        public async Task<string> DeletePermanently(int id)
         {
-            return Delete(id, force: true);
+            return await Delete(id, force: true);
         }
 
-        private string Delete(int id, bool force = false)
+        private async Task<string> Delete(int id, bool force = false)
         {
             var apiEndpoint = String.Format("{0}/{1}", BaseApiEndpoint, id);
             var parameters = new Dictionary<string, string> { { "force", force.ToString().ToLower() } };
-            return Delete<dynamic>(apiEndpoint, parameters).message;
+            return(await  Delete<dynamic>(apiEndpoint, parameters)).message;
         }
     }
 }
